ed51d7ea2aa3e32d4297195460bf4df42b25bc34
David <itsmesquee@gmail.com>
2024-07-31 14:41:54 -0600

    added filter functions

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import './index.css';
 import Home from './Pages/Home';
 import Footer from './componants/Footer'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Books from './Pages/Books';
+import { books } from "./data";
+
 
 
 function App() {
   return (
     <Router>
       <div className='App'>
-        <Route />
         <Nav />
-        <Home />
+        <Route path="/" exact component={Home} />
+        <Route path="/Books" render={() => <Books books={books} />} />
         <Footer />
       </div>
     </Router>
