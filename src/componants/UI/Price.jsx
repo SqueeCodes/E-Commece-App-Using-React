871866591559f5c50103056d5466d506108e781f
David <itsmesquee@gmail.com>
2024-07-31 16:33:07 -0600

    added some functions to route to pages properly

---
+++ src/componants/UI/Price.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Price = ({price}) => {
+  return (
+    <div className="book__price">
+    {book.salePrice ? (
+      <>
+        <span className="book__price--normal">
+          ${book.originalPrice.toFixed(2)}
+        </span>
+        ${book.salePrice.toFixed(2)}
+      </>
+    ) : (
+      <>${book.originalPrice.toFixed(2)}</>
+    )}
+  </div>
+  )
+}
