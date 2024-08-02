e8d6652cbc1213d3d3bcb485fbb0c9640f0d632a
David <itsmesquee@gmail.com>
2024-08-02 10:20:28 -0600

    changed book loading to dynamic and altered app.js function

--- src/App.js
+++ src/App.js
@@ -6,17 +6,18 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Books from './Pages/Books';
 import { books } from "./data";
 import BookInfo from './Pages/BookInfo';
+import Book from './componants/UI/Book';
 
 
 
-function App() {
+function App(Book) {
   return (
     <Router>
       <div className='App'>
         <Nav />
         <Route path="/" exact component={Home} />
         <Route path="/Books" exact render={() => <Books books={books} />} />
-        <Route path="/books/1" render={() => <BookInfo books={books} />} />
+        <Route path={`/books/${Book.id}`} render={() => <BookInfo books={books} />} />
         <Footer />
       </div>
     </Router>
